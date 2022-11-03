d0bb964ee35f5d38b8bb169d449aedc6edd28961
Adrian Américo <1fnadrian1@gmail.com>
2022-11-02 21:36:18 -0300

    feat: add render data on title component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,51 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  listTitles = [
+    {
+      trailer: 'https://www.youtube.com/embed/5qap5aO4i9A',
+      title: 'The Shawshank Redemption',
+      cover: './assets/images/guardios-da-galaxia.jpg',
+      name: 'The Shawshank Redemption',
+      relevance: 1,
+      age: 0,
+      parts: 1,
+      categories: ['Drama', 'Crime'],
+      year: 2000,
+    },
+    {
+      trailer: 'https://www.youtube.com/embed/5qap5aO4i9A',
+      title: 'The Shawshank Redemption 2',
+      cover: './assets/images/guardios-da-galaxia.jpg',
+      name: 'The Shawshank Redemption 2',
+      relevance: 1,
+      age: 12,
+      parts: 1,
+      categories: ['Drama', 'Crime'],
+      year: 2002,
+    },
+    {
+      trailer: 'https://www.youtube.com/embed/5qap5aO4i9A',
+      title: 'The Shawshank Redemption 3',
+      cover: './assets/images/guardios-da-galaxia.jpg',
+      name: 'The Shawshank Redemption 3',
+      relevance: 1,
+      age: 16,
+      parts: 1,
+      categories: ['Drama', 'Crime'],
+      year: 2008,
+    },
+    {
+      trailer: 'https://www.youtube.com/embed/5qap5aO4i9A',
+      title: 'The Shawshank Redemption 4',
+      cover: './assets/images/guardios-da-galaxia.jpg',
+      name: 'The Shawshank Redemption 4',
+      relevance: 1,
+      age: 18,
+      parts: 1,
+      categories: ['Drama', 'Crime'],
+      year: 2008,
+    },
+  ];
+}
--- src/app/shared/list-titles/list-titles.component.ts
+++ src/app/shared/list-titles/list-titles.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-titles',
   templateUrl: './list-titles.component.html',
-  styleUrls: ['./list-titles.component.scss']
+  styleUrls: ['./list-titles.component.scss'],
 })
 export class ListTitlesComponent implements OnInit {
+  @Input()
+  title!: string;
+  @Input()
+  list: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
