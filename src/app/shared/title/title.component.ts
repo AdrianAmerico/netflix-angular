71177958ee9ba26852bea5cc5c173bb14af5d359
Adrian Américo <1fnadrian1@gmail.com>
2022-11-06 05:11:09 -0300

    feat: add playVideo feature

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  ChangeDetectorRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -8,7 +15,32 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TitleComponent implements OnInit {
   @Input()
   title!: any;
-  constructor() {}
+
+  @ViewChild('trailer', { static: false })
+  trailer!: ElementRef<HTMLVideoElement>;
+
+  constructor(private cdRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.cdRef.detectChanges();
+  }
+  public playVideo(): void {
+    this.trailer.nativeElement.currentTime = 0;
+    this.trailer.nativeElement.play();
+  }
+
+  public stopVideo(): void {
+    this.trailer.nativeElement.pause();
+  }
+
+  public getMuted(): boolean {
+    console.log('ué', this.trailer);
+    return this.trailer.nativeElement.muted;
+  }
+
+  public toggleSound(): void {
+    this.trailer.nativeElement.muted = !this.trailer.nativeElement.muted;
+  }
 }
